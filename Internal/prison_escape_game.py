b8894ff031083fefab7a649f1c9d2b997decc3fd
School Account <william.mckee@example.com>
2026-03-11 22:23:37 +1300

    WIP: Coding action fucntion

--- Internal/prison_escape_game.py
+++ Internal/prison_escape_game.py
@@ -6,7 +6,7 @@
 #Hold a maximum of 3 items, hide item under bed to get rid of item or use it
 #Rooms that you can only get to with specific items, such as vents with a
 
-print() #Add space at top
+
 
 #Variables
 instructions = "\nWelcome to PRISON ESCAPE \n" \
@@ -14,12 +14,13 @@ instructions = "\nWelcome to PRISON ESCAPE \n" \
                "Each playthrough the items to help you escape are randomly littered throughout the rooms \n" \
                "Type restart at anytime to retry \n" \
                "Type quit at anytime to stop playing \n" \
-               "Good Luck!!!"
+               "Good Luck!!! \n"
 
 
 rooms = {
     "cell": {
         "description": "You are in your cellroom",
+        "actions": ["Check room for items", "Move room", "Talk to cellmate"],
         "items": ["Sword", "Fork", "Knife"],
         "exits": ["workshop", "bathroom", "cafeteria"]
     },
@@ -59,8 +60,28 @@ class Player:
 
         self.player_location = player_location
         self.room_description = self.player_location["description"]
-        self.items = player_location["items"]
-        self.exits = player_location["exits"]
+        self.player_actions  = self.player_location["actions"]
+        self.items = self.player_location["items"]
+        self.exits = self.player_location["exits"]
+
+    def action(self):
+        while True:
+            for index, action in enumerate(self.player_actions):
+                print(f"{index + 1}: {action}")
+
+            try:
+                choice = int(input("Yo wat good g ")) #FINSIH THIS
+                break
+            except ValueError:
+                print("Please input a valid number")
+        
+        print(choice)
+        if choice == 1:
+            player.look_around()
+        elif choice == 2:
+            player.move_room()
+        else:
+            print("You failed bro") #NEED TO FINSIH OFF THIS IF ELIF ELSE STATEMENT
 
     def look_around(self):
         if len(self.player_location["items"]) > 1:
@@ -73,7 +94,7 @@ class Player:
         else:
             print("You don't find anything")
 
-    def move_room(self):
+    def move_room(self): #NEED TO FIX THIS. When player changes room the exits for that room should show instead of the ones for cell.
         while True:
             for index, value in enumerate(self.exits):
                 print(f"{index + 1}: {value.capitalize()}")
@@ -89,6 +110,9 @@ class Player:
 
                     self.player_location = self.rooms[chosen_room] #Update player location
                     break
+                    
+                else:
+                    print(f"Choose option between 1 and {len(self.exits)}")
 
             except ValueError:
                 print("Please input a valid number")
@@ -98,10 +122,14 @@ class Player:
 
 player = Player(starting_room, rooms)
 
-player.look_around()
-player.move_room()
 
+#----Game Loop----
+print() #Add space at start
+print(instructions) #Give user game instructions
 
-
+while True:
+    player.action()
+    # player.look_around()
+    # player.move_room()
 
 
